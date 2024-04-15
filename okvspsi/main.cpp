fb42e22079f8d14db1580ce9e409ee64126834ef
ShallMate <gw_ling@sjtu.edu.cn>
2024-04-15 21:26:11 +0800

    fix a small bug about sending message

--- okvspsi/main.cpp
+++ okvspsi/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
 {
     oc::CLP cmd(argc, argv);
 
-    //perfOkvsPSI(cmd);
-    networkSocketExampleRun(cmd);
+    perfOkvsPSI(cmd);
+    //networkSocketExampleRun(cmd);
     return 0;
 }
